23ec2bdf891480916ab26d52d2812f7364b8b098
MestiriJacem <mestirijacemm@gmail.com>
2024-04-17 16:35:39 +0200

    nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,18 @@ import { useState,useEffect } from "react";
 import {signIn,signOut, useSession,getProviders} from'next-auth/react'
  export default function Nav () {
     const isUserLoggedIn =true;
+
+    const [providers, setProviders] = useState(null);
+
+    const [toggleDropdown,setToggleDropDown] =useState(false);
+     
+  
+    useEffect(() => {
+      (async () => {
+        const res = await getProviders();
+        setProviders(res);
+      })();
+    }, []);
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
     <Link href='/' className='flex gap-2 flex-center'>
@@ -32,12 +44,52 @@ import {signIn,signOut, useSession,getProviders} from'next-auth/react'
 
            </div>
         ):(
-            <>
-            
-            </>
-        )} 
+          <>
+          {providers &&
+            Object.values(providers).map((provider) => (
+              <button
+                type='button'
+                key={provider.name}
+                onClick={() => {
+                  signIn(provider.id);
+                }}
+                className='black_btn'
+              >
+                Sign in
+              </button>
+            ))}
+        </>
+      )}
+          
+       </div>
+        {/*Desktop navigation */}
+        <div className="sm:hidden flex realative ">
+          {isUserLoggedIn ? (
+            <div className="flex">
+               <Image src="/assets/images/logo.svg" width={37} height={37} className="rounded-full" alt="profile"  onClick={()=>{setToggleDropDown
 
-    </div>
+               }}/>
+
+            </div>
+          ):(
+            <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+          )}
+
+        </div>
     </nav>
   
   
